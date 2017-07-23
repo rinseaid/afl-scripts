c3ac0038d63280921ec48d69238601c025e11249
rinseaid <rinseaid@gmail.com>
2017-07-23 11:45:40 -0400

    Adjusted regex to correct greedy capture

--- afl-rename.sh
+++ afl-rename.sh
@@ -23,17 +23,17 @@ rename -v 's/.*(2[\d]{3}).*Round\s([\d]{1,2})\W{1,3}(.*)\sv\s(.*?)\s(720|x264).*
 #Single-VB
 rename -v 's/.*(2[\d]{3}).*Round\s([\d]{1,2})\W{1,3}(.*)\sv\s(.*?)\s(720|x264).*/$1.sprintf(" - Round ").sprintf("%02d - ",$2).$3.sprintf(" vs. ").$4.sprintf(".mp4")/e' "$renamepath"/*.mp4
 #Split
-rename -v 's/.*(2[\d]{3}).*Round\s([\d]{1,2})\W{1,3}(.*)\sV\s(.*)\s([1-9])[strdnh]{2}.*/$1.sprintf(" - Round ").sprintf("%02d - ",$2).$3.sprintf(" vs. ").$4.sprintf(" Pt").$5.sprintf(".mp4")/e' "$renamepath"/*.mp4
+rename -v 's/.*(2[\d]{3}).*Round\s([\d]{1,2})\W{1,3}(.*)\sV\s(.*?)\s(720|x264).*([1-9])[strdnh]{2}.*/$1.sprintf(" - Round ").sprintf("%02d - ",$2).$3.sprintf(" vs. ").$4.sprintf(" Pt").$5.sprintf(".mp4")/e' "$renamepath"/*.mp4
 #Single
-rename -v 's/.*(2[\d]{3}).*Round\s([\d]{1,2})\W{1,3}(.*)\sv\s(.*)\s.*720.*(?!([1-9])[strdnh]{2}).*/$1.sprintf(" - Round ").sprintf("%02d - ",$2).$3.sprintf(" vs. ").$4.sprintf(".mp4")/e' "$renamepath"/*.mp4
+rename -v 's/.*(2[\d]{3}).*Round\s([\d]{1,2})\W{1,3}(.*)\sv\s(.*?)\s(720|x264).*/$1.sprintf(" - Round ").sprintf("%02d - ",$2).$3.sprintf(" vs. ").$4.sprintf(".mp4")/e' "$renamepath"/*.mp4
 #Split-VB-Finals
 rename -v 's/.*(2[\d]{3}).*([1-9].*Final).*\W{1,3}(.*)\sv\s(.*?)\s(720|x264).*([1-9])[strdnh]{2}.*/$1.sprintf(" - ").$2.sprintf(" - ").$3.sprintf(" vs. ").$4.sprintf(" Pt").$5.sprintf(".mp4")/e' "$renamepath"/*.mp4
 #Single-VB-Finals
 rename -v 's/.*(2[\d]{3}).*([1-9].*Final).*\W{1,3}(.*)\sv\s(.*?)\s(720|x264).*/$1.sprintf(" - ").$2.sprintf(" - ").$3.sprintf(" vs. ").$4.sprintf(".mp4")/e' "$renamepath"/*.mp4
 #Split-Finals
-rename -v 's/.*(2[\d]{3}).*([1-9].*Final)\W{1,3}(.*)\sV\s(.*)\s([1-9])[strdnh]{2}.*/$1.sprintf(" - ").$2.sprintf(" - ").$3.sprintf(" vs. ").$4.sprintf(" Pt").$5.sprintf(".mp4")/e' "$renamepath"/*.mp4
+rename -v 's/.*(2[\d]{3}).*([1-9].*Final)\W{1,3}(.*)\sV\s(.*?)\s(720|x264).*([1-9])[strdnh]{2}.*/$1.sprintf(" - ").$2.sprintf(" - ").$3.sprintf(" vs. ").$4.sprintf(" Pt").$5.sprintf(".mp4")/e' "$renamepath"/*.mp4
 #Single-Finals
-rename -v 's/.*(2[\d]{3}).*([1-9].*Final)\W{1,3}(.*)\sv\s(.*)\s.*720.*(?!([1-9])[strdnh]{2}).*/$1.sprintf(" - ").$2.sprintf(" - ").$3.sprintf(" vs. ").$4.sprintf(".mp4")/e' "$renamepath"/*.mp4
+rename -v 's/.*(2[\d]{3}).*([1-9].*Final)\W{1,3}(.*)\sv\s(.*?)\s(720|x264).*/$1.sprintf(" - ").$2.sprintf(" - ").$3.sprintf(" vs. ").$4.sprintf(".mp4")/e' "$renamepath"/*.mp4
 
 echo "$(date) - moving renamed files to $processpath"
 mv -v *.mp4 "$processpath"
