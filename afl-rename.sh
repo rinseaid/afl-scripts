9a77fa12c458ed45a931de98850276b623e4c4aa
rinseaid <rinseaid@gmail.com>
2017-07-23 10:00:15 -0400

    Updated regex to match 720 and x264 files correctly

--- afl-rename.sh
+++ afl-rename.sh
@@ -19,17 +19,17 @@ find "." -type d -empty -delete -print
 cd "$renamepath"
 echo "$(date) - renaming mp4 files within $renamepath"
 #Split-VB
-rename -v 's/.*(2[\d]{3}).*Round\s([\d]{1,2})\W{1,3}(.*)\sv\s(.*)\s.*(?:720|^(?!720).*x264-VB).*([1-9])[strdnh]{2}.*/$1.sprintf(" - Round ").sprintf("%02d - ",$2).$3.sprintf(" vs. ").$4.sprintf(" Pt").$5.sprintf(".mp4")/e' "$renamepath"/*.mp4
+rename -v 's/.*(2[\d]{3}).*Round\s([\d]{1,2})\W{1,3}(.*)\sv\s(.*?)\s(720|x264).*([1-9])[strdnh]{2}.*/$1.sprintf(" - Round ").sprintf("%02d - ",$2).$3.sprintf(" vs. ").$4.sprintf(" Pt").$5.sprintf(".mp4")/e' "$renamepath"/*.mp4
 #Single-VB
-rename -v 's/.*(2[\d]{3}).*Round\s([\d]{1,2})\W{1,3}(.*)\sv\s(.*)\s.*(?:720|^(?!720).*x264-VB).*/$1.sprintf(" - Round ").sprintf("%02d - ",$2).$3.sprintf(" vs. ").$4.sprintf(".mp4")/e' "$renamepath"/*.mp4
+rename -v 's/.*(2[\d]{3}).*Round\s([\d]{1,2})\W{1,3}(.*)\sv\s(.*?)\s(720|x264).*/$1.sprintf(" - Round ").sprintf("%02d - ",$2).$3.sprintf(" vs. ").$4.sprintf(".mp4")/e' "$renamepath"/*.mp4
 #Split
 rename -v 's/.*(2[\d]{3}).*Round\s([\d]{1,2})\W{1,3}(.*)\sV\s(.*)\s([1-9])[strdnh]{2}.*/$1.sprintf(" - Round ").sprintf("%02d - ",$2).$3.sprintf(" vs. ").$4.sprintf(" Pt").$5.sprintf(".mp4")/e' "$renamepath"/*.mp4
 #Single
 rename -v 's/.*(2[\d]{3}).*Round\s([\d]{1,2})\W{1,3}(.*)\sv\s(.*)\s.*720.*(?!([1-9])[strdnh]{2}).*/$1.sprintf(" - Round ").sprintf("%02d - ",$2).$3.sprintf(" vs. ").$4.sprintf(".mp4")/e' "$renamepath"/*.mp4
 #Split-VB-Finals
-rename -v 's/.*(2[\d]{3}).*([1-9].*Final).*\W{1,3}(.*)\sv\s(.*)\s.*(?:720|^(?!720).*x264-VB).*([1-9])[strdnh]{2}.*/$1.sprintf(" - ").$2.sprintf(" - ").$3.sprintf(" vs. ").$4.sprintf(" Pt").$5.sprintf(".mp4")/e' "$renamepath"/*.mp4
+rename -v 's/.*(2[\d]{3}).*([1-9].*Final).*\W{1,3}(.*)\sv\s(.*?)\s(720|x264).*([1-9])[strdnh]{2}.*/$1.sprintf(" - ").$2.sprintf(" - ").$3.sprintf(" vs. ").$4.sprintf(" Pt").$5.sprintf(".mp4")/e' "$renamepath"/*.mp4
 #Single-VB-Finals
-rename -v 's/.*(2[\d]{3}).*([1-9].*Final).*\W{1,3}(.*)\sv\s(.*)\s.*(?:720|^(?!720).*x264-VB).*/$1.sprintf(" - ").$2.sprintf(" - ").$3.sprintf(" vs. ").$4.sprintf(".mp4")/e' "$renamepath"/*.mp4
+rename -v 's/.*(2[\d]{3}).*([1-9].*Final).*\W{1,3}(.*)\sv\s(.*?)\s(720|x264).*/$1.sprintf(" - ").$2.sprintf(" - ").$3.sprintf(" vs. ").$4.sprintf(".mp4")/e' "$renamepath"/*.mp4
 #Split-Finals
 rename -v 's/.*(2[\d]{3}).*([1-9].*Final)\W{1,3}(.*)\sV\s(.*)\s([1-9])[strdnh]{2}.*/$1.sprintf(" - ").$2.sprintf(" - ").$3.sprintf(" vs. ").$4.sprintf(" Pt").$5.sprintf(".mp4")/e' "$renamepath"/*.mp4
 #Single-Finals
