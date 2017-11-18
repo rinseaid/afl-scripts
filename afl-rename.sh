28f97c1ca30aed23e3e00ed93a1d83f83204d515
rinseaid <richardjhunt@gmail.com>
2017-11-18 16:57:16 -0500

    Add files via upload

--- afl-rename.sh
+++ afl-rename.sh
@@ -19,17 +19,23 @@ find "." -type d -empty -delete -print
 cd "$renamepath"
 echo "$(date) - renaming mp4 files within $renamepath"
 
+echo "$(date) - deleting any pre or post game files within $renamepath"
+find "." -type f \( -name "*Pre Game*" -o -name "*Post Game*" \) -delete -print
+
+echo "$(date) - ensuring all files within $renamepath have word AFL as uppercase"
+rename -v 's/Afl/AFL/g' "$renamepath"/*.mp4
+
 #Split-New
-rename -v 's/.*(2[\d]{3}).*Round\s([\d]{1,2})\W{1,3}(.*)\s[vV]\s(.*[^0-9])\s(?:720.*|x264-VB.*)?\b([1-9])[strdnh]{2}\b.*/$1.sprintf(" - Round ").sprintf("%02d - ",$2).$3.sprintf(" vs. ").$4.sprintf(" Pt").$5.sprintf(".mp4")/e' "$renamepath"/*.mp4
+rename -v 's/\s?([A-Za-z\s]+\W{0,3}[\d]{4})\W{0,3}Round\s([\d]{1,2})\W{1,3}([A-Za-z\s]+)\s[vV]\s\b([A-Za-z\s]+)\b\s(?:720.*|x264-VB.*)?.*([1-9])[strdnh]{2}\b.*/$1.sprintf(" - Round ").sprintf("%02d - ",$2).$3.sprintf(" vs. ").$4.sprintf(" Pt").$5.sprintf(".mp4")/e' "$renamepath"/*.mp4
 
 #Single-New
-rename -v 's/.*(2[\d]{3}).*Round\s([\d]{1,2})\W{1,3}(.*)\s[vV]\s(.*[^0-9])\s(?:720.*|x264-VB.*)?.*/$1.sprintf(" - Round ").sprintf("%02d - ",$2).$3.sprintf(" vs. ").$4.sprintf(".mp4")/e' "$renamepath"/*.mp4
+rename -v 's/\s?([A-Za-z\s]+\W{0,3}[\d]{4})\W{0,3}Round\s([\d]{1,2})\W{1,3}([A-Za-z\s]+)\s[vV]\s\b([A-Za-z\s]+)\b\s(?:720.*|x264-VB.*)?.*/$1.sprintf(" - Round ").sprintf("%02d - ",$2).$3.sprintf(" vs. ").$4.sprintf(".mp4")/e' "$renamepath"/*.mp4
 
 #Split-Finals-New
-rename -v 's/.*(2[\d]{3}).*([1-9].*Final).*\W{1,3}(.*)\s[vV]\s(.*[^0-9])\s(?:720.*|x264-VB.*)?\b([1-9])[strdnh]{2}\b.*/$1.sprintf(" - ").$2.sprintf(" - ").$3.sprintf(" vs. ").$4.sprintf(" Pt").$5.sprintf(".mp4")/e' "$renamepath"/*.mp4
+rename -v 's/\s?([A-Za-z\s]+\W{0,3}[\d]{4})\W{0,3}([1-9][strdnh]{2}.*(?:Final|Test))\W{1,3}([A-Za-z\s]+)\s[vV]\s\b([A-Za-z\s]+)\b\s(?:720.*|x264-VB.*)?.*([1-9])[strdnh]{2}\b.*/$1.sprintf(" - ").$2.sprintf(" - ").$3.sprintf(" vs. ").$4.sprintf(" Pt").$5.sprintf(".mp4")/e' "$renamepath"/*.mp4
 
 #Single-Finals-New
-rename -v 's/.*(2[\d]{3}).*([1-9].*Final)\W{1,3}(.*)\s[vV]\s(.*[^0-9])\s(?:720.*|x264-VB.*)?.*/$1.sprintf(" - ").$2.sprintf(" - ").$3.sprintf(" vs. ").$4.sprintf(".mp4")/e' "$renamepath"/*.mp4
+rename -v 's/\s?([A-Za-z\s]+\W{0,3}[\d]{4})\W{0,3}([1-9][strdnh]{2}.*(?:Final|Test))\W{1,3}([A-Za-z\s]+)\s[vV]\s\b([A-Za-z\s]+)\b\s(?:720.*|x264-VB.*)?.*/$1.sprintf(" - ").$2.sprintf(" - ").$3.sprintf(" vs. ").$4.sprintf(".mp4")/e' "$renamepath"/*.mp4
 
 #Old
 #Split-VB
